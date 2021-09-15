1845271ab17b900fccbef53029eec93e45ad2054
Yuyi <yuyi365@gmail.com>
2021-09-15 15:22:30 -0400

    Completed Assignment

--- test/index-test.js
+++ test/index-test.js
@@ -2,14 +2,14 @@ const { name, height, message } = require("../index.js");
 
 describe("what-is-a-test", () => {
   describe("Name", () => {
-    it('returns "Susan"', () => {
-      expect(name).toEqual("Susan");
+    it('returns "Joe"', () => {
+      expect(name).toEqual("Joe");
     });
   });
 
   describe("Height", () => {
-    it("is less than 40", () => {
-      expect(height).toBeLessThan(40);
+    it("is less than 74", () => {
+      expect(height).toBeLessThan(100);
     });
   });
 
